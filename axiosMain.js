38f6af0dde952a1cb0bd98cb3777efcfddc39dd4
Nikhil <nikhil@writingminds.com>
2019-01-16 14:31:14 +0530

    array updated

--- axiosMain.js
+++ axiosMain.js
@@ -1,26 +1,27 @@
 const cheerio = require("../node_modules/cheerio");
+const _ = require("lodash");
 const axios = require("axios");
 const url = "https://github.com/trending";
 
 const githubTrending = async ur => {
   try {
-    var self = this;
-    self.items = new Array();
+    const jsonData = [];
     const body = await axios.get(ur);
     const $ = cheerio.load(body.data);
     $(".repo-list li").each(function(i, element) {
       const $element = $(element);
       const $title = $element.find("h3 a").text();
       console.log($title);
-      var t = $title.replace(/\n/g, "").trim();
+      var t = _.replace($title, /\n/g, "").trim();
       const $desc = $element.find(".py-1 p").text();
-      var d = $desc.replace(/\n/g, "").trim();
-      self.items[i] = {
+      var d = _.replace($desc, /\n/g, "").trim();
+      const scrap = {
         title: t,
         desc: d
       };
+      jsonData.push(scrap);
     });
-    console.log(self.items);
+    console.log(jsonData);
   } catch (error) {
     console.log(error);
   }
