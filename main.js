b8c71e75b759dfa062e77995df74a205e8b98d82
Nikhil <nikhil@writingminds.com>
2019-01-16 13:47:28 +0530

    asyn-await

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-const cheerio = require('../node_modules/cheerio');
-const request = require('..//node_modules/request');
-const fetch = require('node-fetch');
-const url = 'https://github.com/trending';
+const cheerio = require("../node_modules/cheerio");
+const request = require("..//node_modules/request");
+const fetch = require("node-fetch");
+const url = "https://github.com/trending";
 
-
-const githubTrending = ()=>{
-    return fetch(url).then(response =>{
-        if(response.ok){
-            return response.text()
-        }throw new Error ('Request faild');  
-    },networkError=>{
-        console.log(networkError.message)
-    });    
-}
-githubTrending().then(body =>{
-        var self = this;
-		self.items = new Array();
-    const $ = cheerio.load(body);
-    $('.repo-list li').each(function(i,element){
-        const $element = $(element);
-        const $title = $element.find('h3 a').text()
-        var t = $title.replace(/\n/g , "").trim();
-        const $desc = $element.find('.py-1 p' ).text()
-        var d = $desc.replace(/\n/g , "").trim();
-        // const $star = $element.find('.muted-link aria-label');
-        // console.log($star.text())
-        self.items[i] = {
-            title: t,
-            desc: d
-        };
-    });
-    console.log(self.items) 
+const githubTrending = () => {
+  return fetch(url).then(
+    response => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("Request faild");
+    },
+    networkError => {
+      console.log(networkError.message);
+    }
+  );
+};
+githubTrending().then(body => {
+  var self = this;
+  self.items = new Array();
+  const $ = cheerio.load(body);
+  $(".repo-list li").each(function(i, element) {
+    const $element = $(element);
+    const $title = $element.find("h3 a").text();
+    var t = $title.replace(/\n/g, "").trim();
+    const $desc = $element.find(".py-1 p").text();
+    var d = $desc.replace(/\n/g, "").trim();
+    // const $star = $element.find('.muted-link aria-label');
+    // console.log($star.text())
+    self.items[i] = {
+      title: t,
+      desc: d
+    };
+  });
+  console.log(self.items);
 });
